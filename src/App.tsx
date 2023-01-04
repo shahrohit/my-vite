d80f57bc0f3b5bf0e2c4b5f2d32eb7e6b3a6b765
Rohit Shah <shahrohit2061@gmail.com>
2023-01-04 14:37:32 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+
+  return (
+    <div>
+      <h1 className='text-4xl text-center'>My first React and vite app with tailwind css and typescript 🚀 </h1>
+  </div>
+  )
+}
+
+export default App
